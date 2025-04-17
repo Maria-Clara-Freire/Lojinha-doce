59b21795d000b800383c57a6a7c3d665f8d2c43f
Maria Clara Freire <mariaclaracgfreire@gmail.com>
2025-04-17 14:32:19 -0300

    comiitfinal

--- lojinha-de-doce/src/pages/Detalhe3.jsx
+++ lojinha-de-doce/src/pages/Detalhe3.jsx
@@ -7,31 +7,31 @@ import ProximaViajem from "../components/ProximaViajem.jsx"
 import brigadeiro2 from "../assets/brigadeiro2.jpg"
 
 function Viajem3() {
-  
+
 
   return (
     <>
       <Header />
-      <Nav/>
-  
+      <Nav />
 
-      <CardDentro  nome={brigadeiro2} 
- 
-  descricao= {
-    <>
-   <center>
-       <h1>O NOSSO BRIGADEIRO !!!!</h1>
-       Experimente o sabor irresistível do nosso brigadeiro!<br/>
-          Feito com amor e ingredientes de alta qualidade. Nossa receita tradicional garante um brigadeiro cremoso, doce e delicioso. <br/>
-          Ele vai transportar você para um mundo de sabores nostálgicos.Textura cremosa e suave Sabor doce e intenso, feito com amor e cuidado. Perfeito para qualquer ocasião!<br></br>
-    🍫🍩😉
-    </center>
-    </>
-  } 
-/>
- 
-<ProximaViajem texto="Próximo" caminho="/contato" />
-      <Footer/>
+
+      <CardDentro nome={brigadeiro2}
+
+        descricao={
+          <>
+            <center>
+              <h1>O NOSSO BRIGADEIRO !!!!</h1>
+              Experimente o sabor irresistível do nosso brigadeiro!<br />
+              Feito com amor e ingredientes de alta qualidade. Nossa receita tradicional garante um brigadeiro cremoso, doce e delicioso. <br />
+              Ele vai transportar você para um mundo de sabores nostálgicos.Textura cremosa e suave Sabor doce e intenso, feito com amor e cuidado. Perfeito para qualquer ocasião!<br></br>
+              🍫🍩😉
+            </center>
+          </>
+        }
+      />
+
+      <ProximaViajem texto="Próximo" caminho="/contato" />
+      <Footer />
     </>
   );
 }
